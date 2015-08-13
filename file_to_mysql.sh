beb6018a420ee9e3c7468f129481b915f19e5a0b
alexpan88 <alex.panagiotoy@gmail.com>
2015-08-13 23:37:43 +0300

    file_to_mysql

---
+++ file_to_mysql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## Executes multiple queries from a file to mysql
+
+echo -ne 'Enter the mysql user: '
+read user
+echo -ne 'Enter the mysql user'\''s password: '
+read -s pass
+echo -ne 'Enter the database name: '
+read db
+echo -ne 'Enter the sql file path: '
+read inputfile
+
+while read in
+do
+  echo "$in" |  mysql -u$user -p$pass $db
+done < $inputfile
